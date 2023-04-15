d5f2b0c785d50b7b0717cf71ad3302d8dd4adca1
faizbyp <faizbyp@gmail.com>
2023-04-15 10:03:42 +0700

    using flatlist and adding eslint

--- App.jsx
+++ App.jsx
@@ -1,37 +1,8 @@
+/* eslint-disable react/jsx-no-bind */
 import { useState } from 'react';
-import { StyleSheet, Text, SafeAreaView, View, Button, TextInput } from 'react-native';
-
-export default function App() {
-  const [text, setText] = useState('');
-  const [tasks, setTasks] = useState([]);
-
-  function taskInputHandler(input) {
-    setText(input);
-  }
-
-  function addTaskHandler() {
-    setTasks((currentTasks) => [...currentTasks, text]);
-    setText('');
-  }
-
-  return (
-    <SafeAreaView style={styles.appContainer}>
-      <Text style={styles.heading}>Todo List</Text>
-      <View style={styles.inputContainer}>
-        <TextInput placeholder='input your task' style={styles.textInput} onChangeText={taskInputHandler} />
-        <Button title='Add' style={styles.btn} onPress={addTaskHandler} />
-      </View>
-      <View style={styles.listContainer}>
-        <Text>List of task</Text>
-        {tasks.map((task) => (
-          <View key={task} style={styles.taskItem}>
-            <Text style={styles.taskText}>{task}</Text>
-          </View>
-        ))}
-      </View>
-    </SafeAreaView>
-  );
-}
+import {
+  StyleSheet, Text, SafeAreaView, View, Button, TextInput, FlatList,
+} from 'react-native';
 
 const styles = StyleSheet.create({
   appContainer: {
@@ -43,7 +14,7 @@ const styles = StyleSheet.create({
     textAlign: 'center',
     fontSize: 36,
     fontWeight: 'bold',
-    color: 'gray'
+    color: 'gray',
   },
   inputContainer: {
     flex: 1,
@@ -53,7 +24,7 @@ const styles = StyleSheet.create({
     marginVertical: 8,
     paddingVertical: 4,
     borderBottomWidth: 2,
-    borderBottomColor: '#ccc'
+    borderBottomColor: '#ccc',
   },
   textInput: {
     borderWidth: 2,
@@ -61,7 +32,7 @@ const styles = StyleSheet.create({
     borderRadius: 4,
     padding: 4,
     width: '70%',
-    marginEnd: 4
+    marginEnd: 4,
   },
   listContainer: {
     flex: 5,
@@ -70,10 +41,53 @@ const styles = StyleSheet.create({
     margin: 6,
     padding: 8,
     paddingHorizontal: 16,
-    backgroundColor: 'purple',
+    backgroundColor: 'orange',
     borderRadius: 8,
   },
   taskText: {
-    color: 'white'
-  }
+    color: 'white',
+  },
 });
+
+export default function App() {
+  const [text, setText] = useState('');
+  const [tasks, setTasks] = useState([]);
+
+  function taskInputHandler(input) {
+    setText(input);
+  }
+
+  function addTaskHandler() {
+    setTasks((currentTasks) => [...currentTasks, { task: text, id: Math.random().toString() }]);
+  }
+
+  return (
+    <SafeAreaView style={styles.appContainer}>
+      <Text style={styles.heading}>Todo List</Text>
+      <View style={styles.inputContainer}>
+        <TextInput placeholder="input your task" style={styles.textInput} onChangeText={taskInputHandler} />
+        <Button title="Add" style={styles.btn} onPress={addTaskHandler} />
+      </View>
+      <View style={styles.listContainer}>
+        <Text>List of task</Text>
+        <FlatList
+          data={tasks}
+          renderItem={(itemData) => (
+            <View style={styles.taskItem}>
+              <Text style={styles.taskText}>{itemData.item.task}</Text>
+            </View>
+          )}
+          keyExtractor={(item) => item.id}
+          alwaysBounceVertical={false}
+        />
+        {/* <ScrollView alwaysBounceVertical={false}>
+          {tasks.map((task) => (
+            <View key={task} style={styles.taskItem}>
+              <Text style={styles.taskText}>{task}</Text>
+            </View>
+            ))}
+          </ScrollView> */}
+      </View>
+    </SafeAreaView>
+  );
+}
